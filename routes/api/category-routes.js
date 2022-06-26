318c697309c8c6a52fbdbd6445a5ca77acf1b039
seanxmcdaniel <102200863+seanxmcdaniel@users.noreply.github.com>
2022-06-25 21:04:53 -0400

    Finishing Models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,13 +4,17 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+Category.findAll().then((categoryData) => {
+  res.json(categoryData);
+})
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk({
+    where: {category_name}
+  })
 });
 
 router.post('/', (req, res) => {
